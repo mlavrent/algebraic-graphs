2db2292bca452bc877b028c647afa4610c759410
Mark Lavrentyev <lavrema@outlook.com>
2023-10-06 22:07:01 -0700

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «algebraic-graphs» where
+  -- add package configuration options here
+
+lean_lib «AlgebraicGraphs» where
+  -- add library configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
